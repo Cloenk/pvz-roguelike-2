321c6b3ef50986102d29698805733eeb76b14a04
cloenk <Devin@vdtier.nl>
2026-02-23 11:05:36 +0100

    sun falling from sky

--- pvz-roguelike-2/GameScenes/Game/game.gd
+++ pvz-roguelike-2/GameScenes/Game/game.gd
@@ -1,9 +1,14 @@
 extends Node2D
+
+@onready var sunScene: PackedScene = preload("res://GameScenes/Sun/sun.tscn")
+
 @onready var plants: Node2D = $Ysort/Plants
 @onready var zombies: Node2D = $Ysort/Zombies
 @onready var projectiles: Node2D = $Ysort/Projectiles
 @onready var sun_counter_label: Label = $sunCounter/SunCounterLabel
 @onready var lawn: Lawn = $Lawn
+@onready var sun_timer: Timer = $SunTimer
+@onready var sun_container: Node2D = $SunContainer
 
 var displayedSun = 0.0
 
@@ -12,6 +17,17 @@ func _ready() -> void:
 	GlobalGame.zombies = zombies
 	GlobalGame.projectiles = projectiles
 	GlobalGame.lawn = lawn
+	startLevel()	
+
+func startLevel():
+	sun_timer.start(randf_range(9,11))
+
+func spawnSun():
+	var sun = sunScene.instantiate()
+	sun.global_position = Vector2(randi_range(450,1750),-75)
+	sun_container.add_child(sun)
+	sun.fallFromSky()
+	sun_timer.start(randf_range(9,11))
 
 func _process(delta: float) -> void:
 	displayedSun = lerp(displayedSun,GlobalGame.sun,6*delta)
--- pvz-roguelike-2/GameScenes/Sun/sun.gd
+++ pvz-roguelike-2/GameScenes/Sun/sun.gd
@@ -2,8 +2,10 @@ extends CharacterBody2D
 @onready var back: Sprite2D = $Back
 @onready var backest: Sprite2D = $Backest
 @onready var ap: AnimationPlayer = $AnimationPlayer
+@onready var stop_timer: Timer = $StopTimer
 
 var hasBeenCollected: bool = false
+var isFromSky: bool = false
 
 func _process(delta: float) -> void:
 	back.rotation += 1*delta
@@ -15,9 +17,18 @@ func _process(delta: float) -> void:
 		global_position = lerp(global_position,Vector2(110,125),6.67*delta)
 		if global_position.distance_to(Vector2(110,125)) <= 10:
 			collect()
+	if isFromSky and !hasBeenCollected:
+		global_position.y += 100 * delta
+
+func fallFromSky():
+	isFromSky = true
+	stop_timer.start(randf_range(3,10))
 
 func giveSun():
 	GlobalGame.sun += 50
 
 func collect():
 	ap.play("Delete")
+
+func _on_stop_timer_timeout() -> void:
+	isFromSky = false
