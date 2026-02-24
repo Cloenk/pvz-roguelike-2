c9097633fc0e4c40c319ad88735367da47e08fcc
Devin <246471@student.scalda.nl>
2026-02-24 17:40:40 +0100

    added hit particles

--- pvz-roguelike-2/Globals/GlobalGame.gd
+++ pvz-roguelike-2/Globals/GlobalGame.gd
@@ -1,5 +1,7 @@
 extends Node
 
+@onready var hitParticlesScene: PackedScene = preload("res://Components/HitParticles/hit_particles.tscn")
+
 var sun: float = 1000
 var selectedSeedSlot: SeedSlot
 var isShoveling = false
@@ -7,3 +9,9 @@ var plants
 var zombies
 var projectiles
 var lawn: Lawn
+
+func spawnHitParticles(pos:Vector2,color:Color):
+	var particles = hitParticlesScene.instantiate()
+	particles.global_position = pos
+	particles.modulate = color
+	projectiles.add_child(particles)
--- pvz-roguelike-2/plants/peashooter/pea.gd
+++ pvz-roguelike-2/plants/peashooter/pea.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 @onready var targeting: Targeting = $Targeting
+@onready var ray_cast_2d: RayCast2D = $Targeting/RayCast2D
 
 var damage = 1
 
@@ -7,6 +8,7 @@ func _process(delta: float) -> void:
 	var target:Damage = targeting.getTarget()
 	if target != null:
 		target.damage(damage)
+		GlobalGame.spawnHitParticles(ray_cast_2d.get_collision_point(),Color.GREEN)
 		queue_free()
 
 func _physics_process(delta: float) -> void:
